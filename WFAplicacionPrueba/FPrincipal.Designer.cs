4f32f7ce1d08aaa04dafc8e67c581d8bd75c58a8
DAM 1 <DAM 1@DESKTOP-TKRSI9T>
2024-04-24 10:08:25 +0200

    Agregar archivos de proyecto.

---
+++ WFAplicacionPrueba/FPrincipal.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace WFAplicacionPrueba
+{
+    partial class fPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms, NO TOCAR 
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BVisible = new System.Windows.Forms.Button();
+            this.bEdicion = new System.Windows.Forms.Button();
+            this.BotonActivar = new System.Windows.Forms.Button();
+            this.bTransferir = new System.Windows.Forms.Button();
+            this.lEntrada = new System.Windows.Forms.Label();
+            this.TextBoxEntrada = new System.Windows.Forms.TextBox();
+            this.tdSalida = new System.Windows.Forms.TextBox();
+            this.lSalida = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BVisible
+            // 
+            this.BVisible.Location = new System.Drawing.Point(238, 54);
+            this.BVisible.Name = "BVisible";
+            this.BVisible.Size = new System.Drawing.Size(151, 23);
+            this.BVisible.TabIndex = 0;
+            this.BVisible.Text = "Ver";
+            this.BVisible.UseVisualStyleBackColor = true;
+            this.BVisible.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bEdicion
+            // 
+            this.bEdicion.Location = new System.Drawing.Point(437, 54);
+            this.bEdicion.Name = "bEdicion";
+            this.bEdicion.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.bEdicion.Size = new System.Drawing.Size(151, 23);
+            this.bEdicion.TabIndex = 1;
+            this.bEdicion.Text = "Solo Lectura";
+            this.bEdicion.UseVisualStyleBackColor = true;
+            // 
+            // bActivar
+            // 
+            this.BotonActivar.Location = new System.Drawing.Point(31, 54);
+            this.BotonActivar.Name = "bActivar";
+            this.BotonActivar.Size = new System.Drawing.Size(151, 23);
+            this.BotonActivar.TabIndex = 2;
+            this.BotonActivar.Text = "Activar";
+            this.BotonActivar.UseVisualStyleBackColor = true;
+            this.BotonActivar.Click += new System.EventHandler(this.bActivar_Click);
+            // 
+            // bTransferir
+            // 
+            this.bTransferir.Location = new System.Drawing.Point(618, 54);
+            this.bTransferir.Name = "bTransferir";
+            this.bTransferir.Size = new System.Drawing.Size(151, 23);
+            this.bTransferir.TabIndex = 3;
+            this.bTransferir.Text = "Transferir";
+            this.bTransferir.UseVisualStyleBackColor = true;
+            this.bTransferir.Click += new System.EventHandler(this.bTransferir_Click);
+            // 
+            // lEntrada
+            // 
+            this.lEntrada.AutoSize = true;
+            this.lEntrada.Location = new System.Drawing.Point(47, 126);
+            this.lEntrada.Name = "lEntrada";
+            this.lEntrada.Size = new System.Drawing.Size(74, 13);
+            this.lEntrada.TabIndex = 4;
+            this.lEntrada.Text = "Texto Entrada";
+            this.lEntrada.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tdEntrada
+            // 
+            this.TextBoxEntrada.Enabled = false;
+            this.TextBoxEntrada.Location = new System.Drawing.Point(142, 126);
+            this.TextBoxEntrada.Name = "tdEntrada";
+            this.TextBoxEntrada.Size = new System.Drawing.Size(593, 20);
+            this.TextBoxEntrada.TabIndex = 5;
+            // 
+            // tdSalida
+            // 
+            this.tdSalida.Enabled = false;
+            this.tdSalida.Location = new System.Drawing.Point(142, 174);
+            this.tdSalida.Name = "tdSalida";
+            this.tdSalida.Size = new System.Drawing.Size(593, 20);
+            this.tdSalida.TabIndex = 7;
+            // 
+            // lSalida
+            // 
+            this.lSalida.AutoSize = true;
+            this.lSalida.Location = new System.Drawing.Point(47, 174);
+            this.lSalida.Name = "lSalida";
+            this.lSalida.Size = new System.Drawing.Size(66, 13);
+            this.lSalida.TabIndex = 6;
+            this.lSalida.Text = "Texto Salida";
+            // 
+            // fPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tdSalida);
+            this.Controls.Add(this.lSalida);
+            this.Controls.Add(this.TextBoxEntrada);
+            this.Controls.Add(this.lEntrada);
+            this.Controls.Add(this.bTransferir);
+            this.Controls.Add(this.BotonActivar);
+            this.Controls.Add(this.bEdicion);
+            this.Controls.Add(this.BVisible);
+            this.Name = "fPrincipal";
+            this.Text = "Windows Forms Mi Aplicacion de Prueba";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        // Miembros Privados de la Clase Forms
+
+        private System.Windows.Forms.Button BVisible;
+        private System.Windows.Forms.Button bEdicion;
+        private System.Windows.Forms.Button BotonActivar;
+        private System.Windows.Forms.Button bTransferir;
+        private System.Windows.Forms.Label lEntrada;
+        private System.Windows.Forms.TextBox TextBoxEntrada;
+        private System.Windows.Forms.TextBox tdSalida;
+        private System.Windows.Forms.Label lSalida;
+    }
+}
+
---
+++ WFAplicacionPrueba/FPrincipal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAplicacionPrueba
+{
+    public partial class fPrincipal : Form
+    {
+        public fPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bTransferir_Click(object sender, EventArgs e)
+        {
+            tdSalida.Text = TextBoxEntrada.Text;
+        }
+
+        private void bActivar_Click(object sender, EventArgs e)
+        {
+            if (TextBoxEntrada.Enabled)
+            {
+                TextBoxEntrada.Enabled = false;
+                BotonActivar.Text = "Activar";
+            }
+            else
+            {
+                TextBoxEntrada.Enabled = true;
+                BotonActivar.Text = "Desactivar";
+            }
+            
+
+        }
+    }
+}
